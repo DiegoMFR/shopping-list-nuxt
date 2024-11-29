9783933f5b2a2bc8bad1f1fbc3f982aaa4eb9bc5
Diego Morales <diego.moralesfrancomano@evbox.com>
2024-11-29 16:43:45 +0100

    fetch added

--- server/api/lists.ts
+++ server/api/lists.ts
@@ -1,3 +1,7 @@
-export default defineEventHandler((event) => {
-    return [{name: "Super", id:"1"}]
+export default defineEventHandler(async (event) => {
+    const db = hubDatabase();
+
+    const { results } = await db.prepare('SELECT * from lists').all();
+
+    return results
   })
