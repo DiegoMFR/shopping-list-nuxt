424d8ea8133562b84b41271d2d605bcc8d72825b
Diego Morales <diego.moralesfrancomano@evbox.com>
2024-11-29 13:42:55 +0100

    db added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-  css: ['~/assets/css/main.css']
+  modules: ['@nuxtjs/tailwindcss', '@nuxthub/core'],
+  css: ['~/assets/css/main.css'],
+  hub: {
+    database: true
+  }
 })
